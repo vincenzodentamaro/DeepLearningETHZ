a2ce6de0b64326faf4034d167c535c0da76641e5
Alexander Meulemans <alexander.meulemans@gmail.com>
2018-12-06 10:10:38 +0100

    working on resnet18

--- Dataset.py
+++ Dataset.py
@@ -7,6 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
+import math
 
 
 class PaintingDataset(Dataset):
@@ -23,18 +24,51 @@ class PaintingDataset(Dataset):
         self.info_file = pd.read_csv(csv_file)
         self.root_dir = root_dir
         self.transform = transform
+        self.classes, self.class_to_idx, self.idx_to_class = self.find_classes()
 
     def __len__(self):
-        return len(self.landmarks_frame)
+        return len(self.info_file)
 
     def __getitem__(self, idx):
         img_name = os.path.join(self.root_dir,
                                 self.info_file.at[idx, 'new_filename'])
         image = io.imread(img_name)
         style = self.info_file.at[idx, 'style']
-        sample = {'image': image, 'style': style}
+        class_idx = self.class_to_idx[style]
 
         if self.transform:
-            sample = self.transform(sample)
+            image = self.transform(image)
 
-        return sample
+        return image, class_idx
+
+    def split_train_val(self, val_percentile):
+        """
+        Split dataset in a training set and validation set
+        :param val_percentile: percentage of validation data
+        :return: training dataset and validation dataset
+        """
+        idx_val = math.ceil((1-val_percentile)*len(self))
+        info_file_train = self.info_file.loc[0:idx_val-1, :]
+        info_file_val = self.info_file.loc[idx_val:, :]
+        info_file_train.to_csv('./info_file_train.csv', index=False)
+        info_file_val.to_csv('./info_file_val.csv', index=False)
+
+        data_train = PaintingDataset(csv_file='./info_file_train.csv',root_dir=self.root_dir, transform=self.transform)
+        data_val = PaintingDataset(csv_file='./info_file_val.csv', root_dir=self.root_dir,transform=self.transform)
+
+        return data_train, data_val
+
+    def find_classes(self):
+        df = self.info_file
+        classes = list(df['style'].str.upper().unique())
+        classes.sort()
+        class_to_idx = {val: idx for (idx, val) in enumerate(classes)}
+        idx_to_class = {idx: val for (idx, val) in enumerate(classes)}
+        return classes, class_to_idx, idx_to_class
+
+dataset = PaintingDataset(root_dir='train_reduced',csv_file='final_train_info.csv', transform=None)
+
+test_idx = dataset[5]
+dataset_train, dataset_val = dataset.split_train_val(0.3)
+
+
