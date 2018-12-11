766777e6f68fef85c9dd57c10bbeea7f2fcba1fa
Alexander Meulemans <alexander.meulemans@gmail.com>
2018-12-11 17:11:14 +0100

    resnet working with extra data folder

--- ResNet18.py
+++ ResNet18.py
@@ -6,7 +6,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
-from Dataset import PaintingDataset
+from Dataset import PaintingDataset, AugmentedPaintingDataset
 import os
 import torchvision
 import torchvision.transforms as T
@@ -24,6 +24,9 @@ if __name__ == '__main__':
     parser.add_argument("--resultfile", help='Name of the resultfile that will be created',
                         type=str, default='result_ResNet18.csv')
     parser.add_argument("--datafolder", help='name of the folder with the data', default='train_reduced')
+    parser.add_argument("--datafolder_extra", help='name of the folder with the extra generated data', default='generated_data')
+    parser.add_argument("--infofile_extra", help='name of the csv file with the sample information of the extra '
+                                                 'generated data', default='info_file_gen.csv')
     parser.add_argument("--infofile", help= 'name of the csv file with the sample information', default='final_train_info.csv')
     args = parser.parse_args()
 
@@ -48,10 +51,15 @@ if __name__ == '__main__':
     ## ALL MAIN ARGUMENTS FOR THE SCRIPT ##
 
     # dat_folder = os.path.join(os.path.curdir,'train_reduced')
-    img_folder = os.path.join(os.path.curdir, '../DeepLearningData/train_reduced')
-    data_csv_file = os.path.join(os.path.curdir, 'info_file_testrun.csv')
-    # img_folder = os.path.join(os.path.curdir, args.datafolder)
-    # data_csv_file = os.path.join(os.path.curdir, args.infofile)
+
+    # img_folder = os.path.join(os.path.curdir, '../DeepLearningData/train_reduced')
+    # data_csv_file = os.path.join(os.path.curdir, 'info_file_testrun.csv')
+    # img_folder_extra = os.path.join(os.path.curdir, '../DeepLearningData/train_reduced')
+    # data_csv_file_extra = os.path.join(os.path.curdir, 'info_file_testrun_gen.csv') #TODO comment and uncomment right stuff
+    img_folder_extra = os.path.join(os.path.curdir, args.datafolder_extra)
+    data_csv_file_extra = os.path.join(os.path.curdir, args.infofile_extra)
+    img_folder = os.path.join(os.path.curdir, args.datafolder)
+    data_csv_file = os.path.join(os.path.curdir, args.infofile)
     num_workers = 1
 
 
@@ -63,7 +71,15 @@ if __name__ == '__main__':
     mean_resnet = np.array([0.485, 0.456, 0.406]) # This I found from internet (mean values for ImageNet, we can check if this is correct)
     std_resnet = np.array([0.229, 0.224, 0.225])
 
-    if args.data_augmentation == 'standard':
+    val_transform = T.Compose([
+        T.ToPILImage(),
+        T.Resize(256),  # TODO set this value to 224, so the whole painting is cropped
+        T.CenterCrop(224),
+        T.ToTensor(),
+        T.Normalize(mean_resnet, std_resnet)
+    ])
+
+    if args.data_augmentation == 'extra':
         train_transform = T.Compose([
             T.ToPILImage(),
             T.RandomResizedCrop(224),
@@ -71,38 +87,56 @@ if __name__ == '__main__':
             T.ToTensor(),
             T.Normalize(mean_resnet, std_resnet)
         ])
-    elif args.data_augmentation == 'none':
-        train_transform = T.Compose([
-            T.ToPILImage(),
-            T.Resize(256),  # TODO set this value to 224, so the whole painting is cropped
-            T.CenterCrop(224),
-            T.ToTensor(),
-            T.Normalize(mean_resnet, std_resnet)
-        ])
-    elif args.data_augmentation == 'GAN':
-        raise ImplementationError('GAN data augmentation not yet implemented')
+
+        total_dset = PaintingDataset(root_dir=img_folder, csv_file=data_csv_file, transform=None)
+        extra_dset = PaintingDataset(root_dir = img_folder_extra, csv_file=data_csv_file_extra, transform=None)
+        augmented_dset = AugmentedPaintingDataset(total_dset, extra_dset)
+        train_dset, val_dset, test_dset = augmented_dset.split_train_val_test(0.15, 0.15)
+
+        train_dset.main_dataset.transform = train_transform
+        train_dset.extra_dataset.transform = train_transform
+        val_dset.main_dataset.transform = val_transform
+        val_dset.extra_dataset.transform = val_transform
+        test_dset.main_dataset.transform = val_transform
+        test_dset.extra_dataset.transform = val_transform
+
+        loader_train = DataLoader(train_dset, batch_size=b_size, shuffle=True, num_workers=num_workers)
+        loader_val = DataLoader(val_dset, batch_size=b_size, shuffle=True, num_workers=num_workers)
+        loader_test = DataLoader(test_dset, batch_size=b_size, shuffle=True, num_workers=num_workers)
+
+        nb_classes = len(total_dset.classes)
     else:
-        raise ValueError('data augmentation argument is not recognized, use either "none", "standard" or "GAN"')
-    val_transform = T.Compose([
-        T.ToPILImage(),
-        T.Resize(256), #TODO set this value to 224, so the whole painting is cropped
-        T.CenterCrop(224),
-        T.ToTensor(),
-        T.Normalize(mean_resnet, std_resnet)
-    ])
+        if args.data_augmentation == 'standard':
+            train_transform = T.Compose([
+                T.ToPILImage(),
+                T.RandomResizedCrop(224),
+                T.RandomHorizontalFlip(),
+                T.ToTensor(),
+                T.Normalize(mean_resnet, std_resnet)
+            ])
+        elif args.data_augmentation == 'none':
+            train_transform = T.Compose([
+                T.ToPILImage(),
+                T.Resize(256),  # TODO set this value to 224, so the whole painting is cropped
+                T.CenterCrop(224),
+                T.ToTensor(),
+                T.Normalize(mean_resnet, std_resnet)
+            ])
+        else:
+            raise ValueError('data augmentation argument is not recognized, use either "none", "standard" or "GAN"')
 
-    total_dset = PaintingDataset(root_dir=img_folder,csv_file=data_csv_file,transform=None)
-    train_dset, val_dset, test_dset = total_dset.split_train_val_test(0.15,0.15)
+        total_dset = PaintingDataset(root_dir=img_folder,csv_file=data_csv_file,transform=None)
+        train_dset, val_dset, test_dset = total_dset.split_train_val_test(0.15,0.15)
 
-    train_dset.transform = train_transform
-    val_dset.transform = val_transform
-    test_dset.transform = val_transform
+        train_dset.transform = train_transform
+        val_dset.transform = val_transform
+        test_dset.transform = val_transform
 
-    loader_train = DataLoader(train_dset, batch_size=b_size, shuffle=True, num_workers=num_workers)
-    loader_val = DataLoader(val_dset, batch_size=b_size, shuffle=True, num_workers=num_workers)
-    loader_test = DataLoader(test_dset, batch_size=b_size, shuffle=True, num_workers=num_workers)
+        loader_train = DataLoader(train_dset, batch_size=b_size, shuffle=True, num_workers=num_workers)
+        loader_val = DataLoader(val_dset, batch_size=b_size, shuffle=True, num_workers=num_workers)
+        loader_test = DataLoader(test_dset, batch_size=b_size, shuffle=True, num_workers=num_workers)
 
-    nb_classes = len(total_dset.classes)
+        nb_classes = len(total_dset.classes)
 
     ## IMPLEMENT AND TRAIN RESNET18
 
