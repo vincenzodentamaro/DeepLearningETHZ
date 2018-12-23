de62255073b914969620462f248b0be3a1a16044
Andreas Bally <andreas.bally@gmail.com>
2018-12-23 12:21:13 +0100

    Update ResNet18Nis.py

--- ResNet18Nis.py
+++ ResNet18Nis.py
@@ -133,7 +133,7 @@ import torchvision
 
 # transfer learning on top of ResNet (only replacing final FC layer)
 # model_conv = torchvision.models.resnet18(pretrained=True)
-model_conv = torch.load(resnet.pt)
+model_conv = torch.load('resnet.pt')
 for param in model_conv.parameters():
     param.requires_grad = False
 
