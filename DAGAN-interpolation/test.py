896845018803e2fb8a5ab66e5ea438ddffe0807f
Alexander Meulemans <alexander.meulemans@gmail.com>
2018-12-23 18:07:23 +0100

    debugging

--- DAGAN-interpolation/test.py
+++ DAGAN-interpolation/test.py
@@ -1,5 +1,6 @@
 from data import PaintingsDataset
 
-data = PaintingsDataset(batch_size=32, last_training_class_index=900, reverse_channels=True,
-                                    num_of_gpus=1, gen_batches=10, csv_file=args.infofile, root_dir = args.data_dir)
+data = PaintingsDataset(batch_size=8, last_training_class_index=900, reverse_channels=True,
+                        num_of_gpus=1, gen_batches=8, csv_file='../data_info_files/info_file_testrun_gen.csv',
+                        root_dir = '../../DeepLearningData/train_reduced')
 #init experiment
