a2735773f22edc620581980a238fb6ac77a31bd3
Andreas Bally <andreas.bally@gmail.com>
2019-01-03 17:59:00 +0100

    Update bagan_train.py

--- BAGAN-master/bagan_train.py
+++ BAGAN-master/bagan_train.py
@@ -167,7 +167,7 @@ if __name__ == '__main__':
             )
         for k in range(0,10):
         # Sample and save images
-            img_samples['class_{}'.format(k)] = gan.generate_samples(c=k, samples=1000)
+            img_samples['class_{}'.format(k)] = gan.generate_samples(k, samples=1000)
             np.save('{}/samples_class_{}.npy'.format(res_dir,k),img_samples['class_{}'.format(k)])
             save_image_array(np.array([img_samples['class_{}'.format(k)]]), '{}/plot_class_{}.png'.format(res_dir, k))
 
