63f66bf629e29b050fb92506fc842c2f97e3ee86
Andreas Bally <andreas.bally@gmail.com>
2019-01-04 15:47:35 +0100

    Update classifier_train.py

--- BAGAN-master/classifier_train.py
+++ BAGAN-master/classifier_train.py
@@ -184,8 +184,8 @@ if augmentation==True:
     p = np.random.permutation(len(dataset_x_train_aug))
     dataset_x_train_aug=dataset_x_train_aug[p]
     dataset_y_train_aug=dataset_y_train_aug[p]
-    z=np.zeros((amount, 10))
-    z[np.arange(amount), dataset_y_train_aug] = 1
+    z=np.zeros((amount*10, 10))
+    z[np.arange(amount*10), dataset_y_train_aug] = 1
     dataset_y_train=np.concatenate((dataset_y_train,dataset_y_train_aug),axis=0)
     dataset_y_train=np.concatenate((dataset_y_train,dataset_y_train_aug),axis=0)
 p = np.random.permutation(len(dataset_x_train))
