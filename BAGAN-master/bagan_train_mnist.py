4539014006c6729bfba5fac6c64e50e904af284e
Andreas Bally <andreas.bally@gmail.com>
2019-01-04 22:00:40 +0100

    Update bagan_train_mnist.py

--- BAGAN-master/bagan_train_mnist.py
+++ BAGAN-master/bagan_train_mnist.py
@@ -173,6 +173,6 @@ if __name__ == '__main__':
         # Sample and save images
             temp = gan.generate_samples(k, samples=1000)
             img_samples['class_{}'.format(k)]=temp
-            save_image_array2(np.array([img_samples['class_{}'.format(k)]]), '{}/plot_class_{}.png'.format(res_dir, k))
+            save_image_array2(np.array([img_samples['class_{}'.format(k)]]), '{}/plot_class_{}.png'.format(res_dir, k),1000)
             temp=temp/2+0.5
             np.save('{}/samples_class_{}.npy'.format(res_dir,k),temp)
