fbc341283d8f19a325d3ef2816086ba0c82d48ab
Andreas Bally <andreas.bally@gmail.com>
2019-01-04 21:40:22 +0100

    Update bagan_train_mnist.py

--- BAGAN-master/bagan_train_mnist.py
+++ BAGAN-master/bagan_train_mnist.py
@@ -15,7 +15,7 @@ from optparse import OptionParser
 
 import balancing_gan as bagan
 from rw.batch_generator_mnist import MnistBatchGenerator as BatchGenerator
-from utils import save_image_array2
+from utils import save_image_array
 
 import os
 
