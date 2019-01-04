6f6c4efe73ba0d46991c94636153af883f9badb2
Andreas Bally <andreas.bally@gmail.com>
2019-01-04 20:54:29 +0100

    Update utils.py

--- BAGAN-master/utils.py
+++ BAGAN-master/utils.py
@@ -11,7 +11,29 @@ import numpy as np
 from PIL import Image
 
 
-def save_image_array(img_array, fname,amount):
+def save_image_array(img_array, fname):
+    channels = img_array.shape[2]
+    resolution = img_array.shape[-1]
+    img_rows = img_array.shape[0]
+    img_cols = img_array.shape[1]
+
+    img = np.full([channels, resolution * img_rows, resolution * img_cols], 0.0)
+    for r in range(img_rows):
+        for c in range(img_cols):
+            img[:,
+            (resolution * r): (resolution * (r + 1)),
+            (resolution * (c % 10)): (resolution * ((c % 10) + 1))
+            ] = img_array[r, c]
+
+    img = (img * 127.5 + 127.5).astype(np.uint8)
+    if (img.shape[0] == 1):
+        img = img[0]
+    else:
+        img = np.rollaxis(img, 0, 3)
+
+    Image.fromarray(img).save(fname)
+
+def save_image_array2(img_array, fname,amount):
     channels = img_array.shape[2]
     resolution = img_array.shape[-1]
     img_rows = img_array.shape[0]
@@ -32,4 +54,3 @@ def save_image_array(img_array, fname,amount):
         img = np.rollaxis(img, 0, 3)
 
     Image.fromarray(img).save(fname)
-
