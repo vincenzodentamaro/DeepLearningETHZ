01194445ec84f5398e1adbf6292ba5ea26d8472f
Andreas Bally <andreas.bally@gmail.com>
2019-01-04 21:43:43 +0100

    Update bagan_train_mnist.py

--- BAGAN-master/bagan_train_mnist.py
+++ BAGAN-master/bagan_train_mnist.py
@@ -15,7 +15,7 @@ from optparse import OptionParser
 
 import balancing_gan as bagan
 from rw.batch_generator_mnist import MnistBatchGenerator as BatchGenerator
-from utils import save_image_array
+from utils import save_image_array2
 
 import os
 
@@ -173,4 +173,4 @@ if __name__ == '__main__':
         # Sample and save images
             img_samples['class_{}'.format(k)]=gan.generate_samples(k, samples=1000)
             np.save('{}/samples_class_{}.npy'.format(res_dir,k),img_samples['class_{}'.format(k)])
-            save_image_array(np.array([img_samples['class_{}'.format(k)]]), '{}/plot_class_{}.png'.format(res_dir, k))
+            save_image_array(np.array([img_samples['class_{}'.format(k)]]), '{}/plot_class_{}.png'.format(res_dir, k),1000)
